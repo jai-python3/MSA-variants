ec529b8ff8331deff1cb4fe1f390eec09bcd410b
SaiReddy-A <sai.achakkagari@mcgill.ca>
2022-10-05 16:21:42 -0400

    Update

--- Extract_SNPs_from_MSA.py
+++ Extract_SNPs_from_MSA.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Extract_InDels_from_MSA.py input.fas 
+# Extract_SNPs_from_MSA.py input.fas 
 
 # Copyright (c) 2022 Sai Reddy Achakkagari
 
@@ -14,22 +14,25 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+
 import sys
 from Bio import AlignIO
 y=0
-indel=[]
+snp=[]
 alignment = AlignIO.read(sys.argv[1], "fasta")
 for r in range(0,alignment.get_alignment_length()):
-    if any(record.seq[r] == "-" for record in alignment):
-      indel.append(r)
-      y+=1
-with open("indels.txt",'w') as out:       
-  print('# Number of InDels:', y,file=out)
-  print('POS'.ljust(10),end='\t',file=out)
-  print(" ".join(str(i+1) for i in indel),file=out)
-  for record in alignment:
-    print(record.id[:10].ljust(10),end='\t',file=out)
-    for i in indel:
-      print(record.seq[i],end=' ',file=out)  
-    print(file=out)
+    d=set([record.seq[r] for record in alignment])
+    d.discard('-')
+    if len(d)>1:
+        snp.append(r)
+        y+=1
+with open("snps.txt",'w') as out:   
+    print('# Number of SNPs:', y,file=out)
+    print('POS'.ljust(10),end='\t',file=out)
+    print(" ".join(str(i+1) for i in snp),file=out)
+    for record in alignment:
+        print(record.id[:10].ljust(10),end='\t',file=out)
+        for i in snp:
+            print(record.seq[i],end=' ',file=out)  
+        print(file=out)
 
