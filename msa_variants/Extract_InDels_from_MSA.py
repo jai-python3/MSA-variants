945a015e060eb1d62499b051a2732066604f0c32
Jaideep Sundaram <jai.python3@gmail.com>
2023-11-12 09:48:54 -0500

    chore: changes license info from in-line comments to docstrings

--- msa_variants/Extract_InDels_from_MSA.py
+++ msa_variants/Extract_InDels_from_MSA.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
+"""
+Extract_InDels_from_MSA.py input.fas 
 
-# Extract_InDels_from_MSA.py input.fas 
+Copyright (c) 2023 Jaideep Sundaram
 
-# Copyright (c) 2022 Sai Reddy Achakkagari
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
   
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
 
 import sys
 from Bio import AlignIO
